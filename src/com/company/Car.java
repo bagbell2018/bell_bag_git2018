569e829a8c616639a0c2fc247c20e36d4c6cad81
Alexey <balagurov.ag@gmail.com>
2018-12-25 16:57:05 +0300

    Добавление двух классов Car, Lorry

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -5,8 +5,8 @@ public class Car {
     // Company = Компания, Weight = вес, HorsePower = кол-во лошадиных сил
     Car(){
         company = "Ford";
-        weight = "1373";
-        horsePower = "125";
+        weight = "1682";
+        horsePower = "142";
     }
     Car(String company,String weight, String horsePower){
         this.company = company;
